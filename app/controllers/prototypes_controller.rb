56aa81351c0399013c4bea85f41db9e7329cf826
gray-noizers <hankun970104@gmail.com>
2020-11-16 17:50:41 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,9 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
-  before_action :contributor_confirmation, only:[:edit, :update, :destroy]
+  before_action :set_prototype, except: [:index, :new, :create]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -11,27 +11,23 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    Prototype.create(prototype_params)
-    @prototype = Prototype.create(prototype_params)
+    @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
-      render :index
+      render :new
     end
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -40,9 +36,11 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
@@ -50,8 +48,8 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id )
   end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
 end
--- db/migrate/20201112063656_devise_create_users.rb
+++ db/migrate/20201112063656_devise_create_users.rb
@@ -5,11 +5,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :name,               null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      t.string :name,               null: false
       t.text :profile,              null: false
       t.text :occupation,           null: false
       t.text :position,             null: false
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
